196ba49e61e17c3722e434033b172531d5bfb364
Matt Stanley <Mattstanley2389@gmail.com>
2024-05-06 19:07:14 -0700

    try again lol

--- FAN_NEW_TEST.ino
+++ FAN_NEW_TEST.ino
@@ -1,29 +1,28 @@
-
 // GPIO Pointers
 volatile uint8_t *_portE = (volatile uint8_t *)0x2E;
 volatile uint8_t *_portDDRE = (volatile uint8_t *)0x2D;
 
-const int motorPin1 = 3; // Corresponds to PE3
-const int enablePin = 5; // Corresponds to PE5
+const int motorPin1 = 3;
+const int enablePin = 5; // Corresponds to PE3
 
 void setup()
 {
-    *_portDDRE |= (1 << enablePin); // Set PE5 as output, labeled pin 5
-    *_portDDRE |= (1 << motorPin1); // Set PE3 as output, labeled pin 3
+    *_portDDRE |= (1 << motorPin1); // Set PE5 as output
+    *_portDDRE |= (1 << enablePin); // Set PE3 as output
 
-    // start pins low
-    *_portE &= ~(1 << enablePin); // Set PE5 low
-    *_portE &= ~(1 << motorPin1); // Set PE3 low
+    // Ensure both pins are initially low
+    *_portE &= ~(1 << motorPin1); // Set PE5 low
+    *_portE &= ~(1 << enablePin); // Set PE3 low
 }
 
 void loop()
 {
-    *_portE |= (1 << motorPin1); // Set PE3 high
-    *_portE |= (1 << enablePin); // Set PE5 high
+    *_portE |= (1 << enablePin); // Set PE3 high
+    *_portE |= (1 << motorPin1); // Set PE5 high
     delay(5000);
 
     // Turn off fan
-    *_portE &= ~(1 << enablePin); // Set PE5 low
-    *_portE &= ~(1 << motorPin1); // Set PE3 low
+    *_portE &= ~(1 << motorPin1); // Set PE5 low
+    *_portE &= ~(1 << enablePin); // Set PE3 low
     delay(5000);
 }
